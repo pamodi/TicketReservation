400c4ff6b0421416c2d41e73583c7c6d332afcb0
Pamodi <pamo9055@gmail.com>
2025-03-12 12:01:28 -0400

    Add export option to reservation list page

--- TicketTracker/Ticket.UI/wwwroot/js/site.js
+++ TicketTracker/Ticket.UI/wwwroot/js/site.js
@@ -31,3 +31,12 @@ window.restrictPhoneInput = () => {
 		this.value = this.value.replace(/\D/g, ''); // Remove non-numeric characters
 	});
 };
+
+function saveAsFile(filename, contentBase64) {
+	const link = document.createElement('a');
+	link.href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + contentBase64;
+	link.download = filename;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+}
